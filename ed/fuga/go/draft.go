87a2ca69c7b9399af8f2622a088b7750c8630540
André Luiz <andreluizdasilvacruz@gmail.com>
2026-04-03 01:29:30 -0300

    refiz a da fuga

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -3,31 +3,31 @@ package main
 import "fmt"
 
 func main() {
-
     var h, p, f, d int
 
     fmt.Scan(&h, &p, &f, &d)
 
     for {
-
         if f == h {
-            fmt.Printf("S\n")
+            fmt.Println("S")
             break
-        }
+        } 
 
         if f == p {
-            fmt.Printf("N\n")
+            fmt.Println("N")
             break
         }
 
         f = f + (d)
 
-        if f > 15 {
-            f = 0
-        }
-
         if f < 0 {
             f = 15
         }
+
+        if f > 15 {
+            f = 0
+        }
     }
+
+
 }
